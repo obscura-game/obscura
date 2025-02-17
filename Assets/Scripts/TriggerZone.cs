4fd896b2353af98a903b878033c3fe5dafc26108
MichaelSani05 <msmichaelsani@gmail.com>
2025-02-17 11:52:46 +0100

    Trgier Zone para desencadenar eventos

---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    {
+        
+    }dw a
+}
+d    // Update d called once per frame
+    void Update()
